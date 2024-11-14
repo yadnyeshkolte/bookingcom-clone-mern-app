ac55985524f8c4d4c5fafe8989c0db81c387a123
yadnyeshkolte <yadnyeshkolte@gmail.com>
2024-11-14 15:42:21 +0530

    Project files updated to Server is running on port 7000

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/test', async (req: Request, res: Response) => {
+  res.json({ message: "Hello from express endpoint" });
+});
+
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+});
+
